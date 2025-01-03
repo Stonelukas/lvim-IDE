474a83f80d3f9fe6491192d8fefb60af23ae2861
stonelukas <163901297+Stonelukas@users.noreply.github.com>
2025-01-03 10:57:34 +0100

    Config for "gitlinker"

--- lua/modules/user/configs/version_control/init.lua
+++ lua/modules/user/configs/version_control/init.lua
@@ -1,3 +1,33 @@
 local config = {}
 
+function config.gitlinker()
+    require("gitlinker").setup({
+        opts = {
+            remote = nil,
+            add_current_line_on_normal_mode = true,
+            action_callback = require("gitlinker.actions").copy_to_clipboard,
+            print_url = true,
+        },
+        callbacks = {
+            ["github.com"] = require("gitlinker.hosts").get_github_type_url,
+        },
+        mappings = "<leader>gy",
+        config = function()
+            require("gitlinker").setup()
+            vim.keymap.set(
+                "n",
+                "<leader>go",
+                '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+                { desc = "Open Permalink in Browser", silent = true }
+            )
+            vim.keymap.set(
+                "v",
+                "<leader>go",
+                '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+                { desc = "Open Permalink in Browser" }
+            )
+        end,
+    })
+end
+
 return config
